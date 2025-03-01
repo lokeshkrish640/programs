b3154dc7e8389d44079055d7abcf7f7b586d6ac9
lokeshkrish640 <e0323022@sriher.edu.in>
2025-03-01 14:05:24 +0530

    Create ASCII value.java

---
+++ ASCII value.java
@@ -0,0 +1,9 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        char ch = 'A';
+        int castAscii = (int) ch;
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+    }
+}
